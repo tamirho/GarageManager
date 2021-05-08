ce51d29b7055018e1e79670fcc2db29df95efe79
tamirho <tamirh1994@gmail.com>
2021-05-08 20:07:19 +0300

    add all Vehicles classes

--- Ex03.GarageLogic/VehicleParts/Wheel.cs
+++ Ex03.GarageLogic/VehicleParts/Wheel.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ex03.GarageLogic.Exceptions;
 
 namespace Ex03.GarageLogic.VehicleParts
 {
     internal class Wheel
     {
+        private string m_ManufacturerName;
+        private float m_CurrentAirPressure;
+        private float m_MaxAirPressure;
+
+        public Wheel(string i_ManufacturerName, float i_CurrentAirPressure, float i_MaxAirPressure)
+        {
+            ManufacturerName = i_ManufacturerName;
+            MaxAirPressure = i_MaxAirPressure;
+            CurrentAirPressure = i_CurrentAirPressure;
+        }
+
+        public void AddAirPressure(float i_AirPressure)
+        {
+            CurrentAirPressure = m_CurrentAirPressure + i_AirPressure;
+        }
+
+        public string ManufacturerName
+        {
+            get
+            {
+                return m_ManufacturerName;
+            }
+            set
+            {
+                
+                m_ManufacturerName = value;
+            }
+        }
+
+        public float CurrentAirPressure
+        {
+            get
+            {
+                return m_CurrentAirPressure;
+            }
+            set
+            {
+                if (value < 0 || value > m_MaxAirPressure)
+                {
+                    throw new ValueOutOfRangeException("Invalid CurrentAirPressure ", value, 0, m_MaxAirPressure);
+                }
+
+                m_CurrentAirPressure = value;
+            }
+        }
+
+        public float MaxAirPressure
+        {
+            get
+            {
+                return m_MaxAirPressure;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("MaxAirPressure must be a positive integer");
+                }
+
+                m_MaxAirPressure = value;
+            }
+        }
     }
 }
--- Ex03.GarageLogic/Vehicles/Bike.cs
+++ Ex03.GarageLogic/Vehicles/Bike.cs
@@ -1,12 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ex03.GarageLogic.VehicleParts;
 
 namespace Ex03.GarageLogic.Vehicles
 {
     public class Bike : Vehicle
     {
+        private eLicenseType m_LicenseType;
+        private int m_EngineVolume;
+
+        internal Bike(
+            string i_LicenseNumber,
+            string i_ModelName,
+            List<Wheel> i_Wheels,
+            EnergyUnit i_EnergyUnit,
+            eLicenseType i_LicenseType,
+            int i_EngineVolume)
+            : base(i_LicenseNumber, i_ModelName, i_Wheels, i_EnergyUnit)
+        {
+            LicenseType = i_LicenseType;
+            EngineVolume = i_EngineVolume;
+        }
+
+        public enum eLicenseType
+        {
+            A,
+            AA,
+            B1,
+            BB
+        }
+
+        public eLicenseType LicenseType
+        {
+            get
+            {
+                return m_LicenseType;
+            }
+            set
+            {
+                if(!Enum.IsDefined(typeof(eLicenseType), value))
+                {
+                    throw new ArgumentException("Error with eLicenseType conversion");
+                }
+
+                m_LicenseType = value;
+            }
+        }
+
+        public int EngineVolume
+        {
+            get
+            {
+                return m_EngineVolume;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("EngineVolume must be a positive integer");
+                }
+                
+                m_EngineVolume = value;
+            }
+        }
+
     }
 }
