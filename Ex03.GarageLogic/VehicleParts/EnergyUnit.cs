a9e867318487b119c3c977189bda46bb66e4eb1b
tamirho <tamirh1994@gmail.com>
2021-05-08 17:31:10 +0300

    Add project files.

---
+++ Ex03.GarageLogic/VehicleParts/EnergyUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic.VehicleParts
+{
+    internal abstract class EnergyUnit
+    {
+
+    }
+}
