4287b1d45a6e4b91e7766b6ee094756846f4a6d1
Yuval <yakovskind@gmail.com>
2021-05-16 11:57:27 +0300

    init

---
+++ Ex03.ConsoleUI/GarageMangerUI.cs
@@ -0,0 +1,15 @@
+﻿using Ex03.GarageLogic;
+namespace Ex03.ConsoleUI
+{
+    public class GarageMangerUI
+    {
+        private readonly GarageManager m_GarageManager;
+
+        public GarageMangerUI()
+        {
+            m_GarageManager = new GarageManager();
+        }
+
+
+    }
+}
